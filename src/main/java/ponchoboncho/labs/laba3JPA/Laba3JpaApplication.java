922c25f54632a40eb0bffaffd287b7c6c482f157
poncho_boncho <Sorkolas@mail.com>
2023-12-18 22:08:46 +0300

    Initial commit

---
+++ src/main/java/ponchoboncho/labs/laba3JPA/Laba3JpaApplication.java
@@ -0,0 +1,13 @@
+package ponchoboncho.labs.laba3JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba3JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ponchoboncho/labs/laba3JPA/Laba3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ponchoboncho.labs.laba3JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
